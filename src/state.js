bf31a6530744bb134bfa4e6ca2a6a27fac276005
Raptor4 <raptor4mc@gmail.com>
2026-04-05 10:34:24 +0200

    Bust module cache for main entry after syntax fix

--- src/state.js
+++ src/state.js
@@ -3,18 +3,25 @@ const THREE = window.THREE;
 export const worldState = {
     selectedBlockIndex: 0,
     worldBlocks: new Map(),
+    worldBlockList: [],
+    collidableBlockList: [],
+    collidableBlocks: new Set(),
+    blockCoordsByKey: new Map(),
     chunkBlocks: new Map(),
     chunkFaceQuads: new Map(),
     chunkMeta: new Map(),
     dirtyChunks: new Set(),
     permanentBlocks: new Map(),
     permanentBlocksByChunk: new Map(),
+    topSolidHeightByColumn: new Map(),
     loadedChunks: new Set(),
-    simplex: new window.SimplexNoise()
+    simplex: new window.SimplexNoise(),
+    frame: 0,
+    frameCameraAxial: { q: 0, r: 0, h: 0 }
 };
 
 export const inputState = {
-    keys: {},
+    keys: new Uint8Array(256),
     isLocked: false,
     pitch: 0,
     yaw: 0,
