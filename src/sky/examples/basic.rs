fa02703fe7eac500e38c994a5fd42413359009ed
Raptor4 <raptor4mc@gmail.com>
2026-04-06 10:12:22 +0200

    Create basic.rs

---
+++ src/sky/examples/basic.rs
@@ -0,0 +1,40 @@
+use bevy::prelude::*;
+use bevy_flycam::{FlyCam, NoCameraPlayerPlugin};
+use bevy_sky_gradient::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_plugins(NoCameraPlayerPlugin)
+        .add_plugins(SkyPlugin::default())
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // circular base
+    commands.spawn((
+        Mesh3d(meshes.add(Circle::new(4.0))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+    ));
+    // cube
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
+
+    // camera
+    commands.spawn((
+        // tell SkyPlugin we want the skybox centered on this camera
+        SkyboxMagnetTag,
+        Camera3d::default(),
+        Transform::from_xyz(-0.4, 0.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        FlyCam,
+    ));
+}
