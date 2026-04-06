190aca549515f8cde3762f7ff8b9edddf4bd86ae
Raptor4 <raptor4mc@gmail.com>
2026-04-06 10:08:21 +0200

    Create aurora_material.rs

---
+++ src/sky/src/aurora_material.rs
@@ -0,0 +1,51 @@
+use bevy::mesh::MeshVertexBufferLayoutRef;
+use bevy::pbr::{MaterialPipeline, MaterialPipelineKey};
+use bevy::prelude::*;
+use bevy::render::render_resource::{
+    AsBindGroup, CompareFunction, RenderPipelineDescriptor, SpecializedMeshPipelineError,
+};
+use bevy::shader::ShaderRef;
+
+use crate::bind_groups::AuroraBindGroup;
+
+#[derive(Asset, AsBindGroup, Reflect, Debug, Clone)]
+pub struct AuroraMaterial {
+    #[uniform(0)]
+    pub aurora_settings: crate::bind_groups::AuroraBindGroup,
+
+    #[texture(1, dimension = "3d")]
+    #[sampler(2)]
+    pub noise3_image: Handle<Image>,
+}
+
+impl Material for AuroraMaterial {
+    fn vertex_shader() -> ShaderRef {
+        crate::assets::FULL_AURORA_SHADER_HANDLE.into()
+    }
+    fn fragment_shader() -> ShaderRef {
+        crate::assets::FULL_AURORA_SHADER_HANDLE.into()
+    }
+
+    fn specialize(
+        _pipeline: &MaterialPipeline,
+        descriptor: &mut RenderPipelineDescriptor,
+        _layout: &MeshVertexBufferLayoutRef,
+        _key: MaterialPipelineKey<Self>,
+    ) -> Result<(), SpecializedMeshPipelineError> {
+        if let Some(depth_stencil) = &mut descriptor.depth_stencil {
+            depth_stencil.depth_write_enabled = false;
+            depth_stencil.depth_compare = CompareFunction::Always;
+        }
+
+        Ok(())
+    }
+}
+
+impl Default for AuroraMaterial {
+    fn default() -> Self {
+        AuroraMaterial {
+            aurora_settings: AuroraBindGroup::default(),
+            noise3_image: Handle::default(),
+        }
+    }
+}
