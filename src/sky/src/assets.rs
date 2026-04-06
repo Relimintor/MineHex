2a3622404fc7dd0e2b224542d98e8dbbe029b84c
Raptor4 <raptor4mc@gmail.com>
2026-04-06 10:07:37 +0200

    Create assets.rs

---
+++ src/sky/src/assets.rs
@@ -0,0 +1,102 @@
+use bevy::{asset::uuid_handle, prelude::*};
+
+pub const SKY_SHADER_PATH: &str = "shaders/full_sky.wgsl";
+pub const SKY_SHADER_HANDLE: Handle<Shader> = uuid_handle!("0aed3aa7-55d3-43be-9e04-5637b0e9ceef");
+pub const GRADIENT_SHADER_PATH: &str = "shaders/gradient.wgsl";
+pub const GRADIENT_SHADER_HANDLE: Handle<Shader> =
+    uuid_handle!("0aed3aa1-15d3-42be-9e04-5637b0e9cefc");
+pub const AURORA_SHADER_PATH: &str = "shaders/aurora.wgsl";
+pub const AURORA_SHADER_HANDLE: Handle<Shader> =
+    uuid_handle!("0aed3aa1-15d3-42be-9e03-2137b0eecbfc");
+pub const FULL_AURORA_SHADER_PATH: &str = "shaders/full_aurora.wgsl";
+pub const FULL_AURORA_SHADER_HANDLE: Handle<Shader> =
+    uuid_handle!("0aed3aa1-15d3-42be-9e03-2731b4eecbfb");
+pub const SUN_SHADER_PATH: &str = "shaders/sun.wgsl";
+pub const SUN_SHADER_HANDLE: Handle<Shader> = uuid_handle!("0aed3aa1-15d3-42be-9e03-2137b0e2c3fb");
+pub const STARS_SHADER_PATH: &str = "shaders/stars.wgsl";
+pub const STARS_SHADER_HANDLE: Handle<Shader> =
+    uuid_handle!("1a3d3ae1-15d3-42be-9e03-2137b0e2c3fb");
+pub const NOISE_SHADER_PATH: &str = "shaders/noise.wgsl";
+pub const NOISE_SHADER_HANDLE: Handle<Shader> =
+    uuid_handle!("1a3d3ae1-15d3-42be-9e03-2131b0e5c2fe");
+pub const FULL_GRADIENT_SHADER_PATH: &str = "shaders/full_gradient.wgsl";
+pub const FULL_GRADIENT_SHADER_HANDLE: Handle<Shader> =
+    uuid_handle!("1a3d3ae1-15d3-42be-9e03-2131b0e3c1ef");
+
+pub struct SkyAssetsPlugin;
+
+impl Plugin for SkyAssetsPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, initialize_shaders);
+    }
+}
+
+pub fn initialize_shaders(mut shaders: ResMut<Assets<Shader>>) {
+    let _result = shaders.insert(
+        &SKY_SHADER_HANDLE,
+        Shader::from_wgsl(
+            String::from_utf8(include_bytes!("../assets/shaders/full_sky.wgsl").into())
+                .expect(format!("'{}' shader is not valid UTF-8", SKY_SHADER_PATH).as_str()),
+            "bevy_sky_gradient/shaders/full_sky.wgsl",
+        ),
+    );
+    let _result = shaders.insert(
+        &GRADIENT_SHADER_HANDLE,
+        Shader::from_wgsl(
+            String::from_utf8(include_bytes!("../assets/shaders/gradient.wgsl").into())
+                .expect(format!("'{}' shader is not valid UTF-8", GRADIENT_SHADER_PATH).as_str()),
+            "bevy_sky_gradient/shaders/gradient.wgsl",
+        ),
+    );
+    let _result = shaders.insert(
+        &FULL_GRADIENT_SHADER_HANDLE,
+        Shader::from_wgsl(
+            String::from_utf8(include_bytes!("../assets/shaders/full_gradient.wgsl").into())
+                .expect(
+                    format!("'{}' shader is not valid UTF-8", FULL_GRADIENT_SHADER_PATH).as_str(),
+                ),
+            "bevy_sky_gradient/shaders/full_gradient.wgsl",
+        ),
+    );
+    let _result = shaders.insert(
+        &AURORA_SHADER_HANDLE,
+        Shader::from_wgsl(
+            String::from_utf8(include_bytes!("../assets/shaders/aurora.wgsl").into())
+                .expect(format!("'{}' shader is not valid UTF-8", AURORA_SHADER_PATH).as_str()),
+            "bevy_sky_gradient/shaders/aurora.wgsl",
+        ),
+    );
+    let _result = shaders.insert(
+        &FULL_AURORA_SHADER_HANDLE,
+        Shader::from_wgsl(
+            String::from_utf8(include_bytes!("../assets/shaders/full_aurora.wgsl").into()).expect(
+                format!("'{}' shader is not valid UTF-8", FULL_AURORA_SHADER_PATH).as_str(),
+            ),
+            "bevy_sky_gradient/shaders/full_aurora.wgsl",
+        ),
+    );
+    let _result = shaders.insert(
+        &STARS_SHADER_HANDLE,
+        Shader::from_wgsl(
+            String::from_utf8(include_bytes!("../assets/shaders/stars.wgsl").into())
+                .expect(format!("'{}' shader is not valid UTF-8", STARS_SHADER_PATH).as_str()),
+            "bevy_sky_gradient/shaders/stars.wgsl",
+        ),
+    );
+    let _result = shaders.insert(
+        &SUN_SHADER_HANDLE,
+        Shader::from_wgsl(
+            String::from_utf8(include_bytes!("../assets/shaders/sun.wgsl").into())
+                .expect(format!("'{}' shader is not valid UTF-8", SUN_SHADER_PATH).as_str()),
+            "bevy_sky_gradient/shaders/sun.wgsl",
+        ),
+    );
+    let _result = shaders.insert(
+        &NOISE_SHADER_HANDLE,
+        Shader::from_wgsl(
+            String::from_utf8(include_bytes!("../assets/shaders/noise.wgsl").into())
+                .expect(format!("'{}' shader is not valid UTF-8", NOISE_SHADER_PATH).as_str()),
+            "bevy_sky_gradient/shaders/noise.wgsl",
+        ),
+    );
+}
